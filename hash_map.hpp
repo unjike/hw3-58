8c9c981160cab0cfdde05a38e83911cf48af2082
Sahil-Patil-23 <140925512+Sahil-Patil-23@users.noreply.github.com>
2025-04-09 14:44:32 -0700

    Update hash_map.hpp

--- hash_map.hpp
+++ hash_map.hpp
@@ -1,88 +1,3 @@
-// #pragma once
-
-// #include "kmer_t.hpp"
-// #include <upcxx/upcxx.hpp>
-
-// struct HashMap {
-//     std::vector<kmer_pair> data;
-//     std::vector<int> used;
-
-//     size_t my_size;
-
-//     size_t size() const noexcept;
-
-//     HashMap(size_t size);
-
-//     // Most important functions: insert and retrieve
-//     // k-mers from the hash table.
-//     bool insert(const kmer_pair& kmer);
-//     bool find(const pkmer_t& key_kmer, kmer_pair& val_kmer);
-
-//     // Helper functions
-
-//     // Write and read to a logical data slot in the table.
-//     void write_slot(uint64_t slot, const kmer_pair& kmer);
-//     kmer_pair read_slot(uint64_t slot);
-
-//     // Request a slot or check if it's already used.
-//     bool request_slot(uint64_t slot);
-//     bool slot_used(uint64_t slot);
-// };
-
-// HashMap::HashMap(size_t size) {
-//     my_size = size;
-//     data.resize(size);
-//     used.resize(size, 0);
-// }
-
-// bool HashMap::insert(const kmer_pair& kmer) {
-//     uint64_t hash = kmer.hash();
-//     uint64_t probe = 0;
-//     bool success = false;
-//     do {
-//         uint64_t slot = (hash + probe++) % size();
-//         success = request_slot(slot);
-//         if (success) {
-//             write_slot(slot, kmer);
-//         }
-//     } while (!success && probe < size());
-//     return success;
-// }
-
-// bool HashMap::find(const pkmer_t& key_kmer, kmer_pair& val_kmer) {
-//     uint64_t hash = key_kmer.hash();
-//     uint64_t probe = 0;
-//     bool success = false;
-//     do {
-//         uint64_t slot = (hash + probe++) % size();
-//         if (slot_used(slot)) {
-//             val_kmer = read_slot(slot);
-//             if (val_kmer.kmer == key_kmer) {
-//                 success = true;
-//             }
-//         }
-//     } while (!success && probe < size());
-//     return success;
-// }
-
-// bool HashMap::slot_used(uint64_t slot) { return used[slot] != 0; }
-
-// void HashMap::write_slot(uint64_t slot, const kmer_pair& kmer) { data[slot] = kmer; }
-
-// kmer_pair HashMap::read_slot(uint64_t slot) { return data[slot]; }
-
-// bool HashMap::request_slot(uint64_t slot) {
-//     if (used[slot] != 0) {
-//         return false;
-//     } else {
-//         used[slot] = 1;
-//         return true;
-//     }
-// }
-
-// size_t HashMap::size() const noexcept { return my_size; }
-
-
 #pragma once
 
 #include "kmer_t.hpp"
@@ -96,24 +11,66 @@ struct HashMap {
 
     size_t my_size;
 
-    size_t size() const noexcept;
+    size_t size() const noexcept { return size_g_; }
 
     HashMap(size_t size);
 
     // Most important functions: insert and retrieve
     // k-mers from the hash table.
-    bool insert(const kmer_pair& kmer);
-    bool find(const pkmer_t& key_kmer, kmer_pair& val_kmer);
+    bool insert(const kmer_pair& kmer){
+        uint64_t hash = kmer.hash();
+        uint64_t probe = 0;
+        bool success = false;
+        do {
+            uint64_t slot = (hash + probe++) % size();
+            success = request_slot(slot);
+            if (success) {
+                write_slot(slot, kmer);
+            }
+        } while (!success && probe < size());
+        return success;
+    }
+
+    bool find(const pkmer_t& key_kmer, kmer_pair& val_kmer){
+        uint64_t hash = key_kmer.hash();
+        uint64_t probe = 0;
+        bool success = false;
+        do {
+            uint64_t slot = (hash + probe++) % size();
+            if (slot_used(slot)) {
+                val_kmer = read_slot(slot);
+                if (val_kmer.kmer == key_kmer) {
+                    success = true;
+                }
+            }
+        } while (!success && probe < size());
+        return success;
+    }
 
     // Helper functions
 
     // Write and read to a logical data slot in the table.
-    void write_slot(uint64_t slot, const kmer_pair& kmer);
-    kmer_pair read_slot(uint64_t slot);
+    void write_slot(uint64_t slot, const kmer_pair& kmer){
+        auto slot_ptr = data[slot/stride_]+slot%stride_;
+        upcxx::rput(kmer, slot_ptr).wait();
+    }
+
+    kmer_pair read_slot(uint64_t slot){
+        auto slot_ptr = data[slot/stride_]+slot%stride_;
+        return upcxx::rget(slot_ptr).wait();    
+    }
 
     // Request a slot or check if it's already used.
-    bool request_slot(uint64_t slot);
-    bool slot_used(uint64_t slot);
+    bool request_slot(uint64_t slot){
+        auto slot_ptr = used[slot/stride_]+slot%stride_;
+        int slot_val = ad_.fetch_add(slot_ptr, 1, std::memory_order_relaxed).wait();
+        return (slot_val == 0);
+    }
+
+    bool slot_used(uint64_t slot){
+        auto slot_ptr = used[slot/stride_]+slot%stride_;
+        return upcxx::rget(slot_ptr).wait() != 0;
+    }
 
 private:
     int N_g_, n_;
@@ -142,57 +99,57 @@ HashMap::HashMap(size_t size)
     }
 }
 
-bool HashMap::insert(const kmer_pair& kmer) {
-    uint64_t hash = kmer.hash();
-    uint64_t probe = 0;
-    bool success = false;
-    do {
-        uint64_t slot = (hash + probe) % size();
-        success = request_slot(slot);
-        if (success) {
-            write_slot(slot, kmer);
-        }
-        ++probe;
-    } while (!success && probe < size());
-    return success;
-}
+// bool HashMap::insert(const kmer_pair& kmer) {
+//     uint64_t hash = kmer.hash();
+//     uint64_t probe = 0;
+//     bool success = false;
+//     do {
+//         uint64_t slot = (hash + probe) % size();
+//         success = request_slot(slot);
+//         if (success) {
+//             write_slot(slot, kmer);
+//         }
+//         ++probe;
+//     } while (!success && probe < size());
+//     return success;
+// }
 
-bool HashMap::find(const pkmer_t& key_kmer, kmer_pair& val_kmer) {
-    uint64_t hash = key_kmer.hash();
-    uint64_t probe = 0;
-    bool success = false;
-    do {
-        uint64_t slot = (hash + probe) % size();
-        if (slot_used(slot)) {
-            val_kmer = read_slot(slot);
-            if (val_kmer.kmer == key_kmer) {
-                success = true;
-            }
-        }
-        ++probe;
-    } while (!success && probe < size());
-    return success;
-}
+// bool HashMap::find(const pkmer_t& key_kmer, kmer_pair& val_kmer) {
+//     uint64_t hash = key_kmer.hash();
+//     uint64_t probe = 0;
+//     bool success = false;
+//     do {
+//         uint64_t slot = (hash + probe) % size();
+//         if (slot_used(slot)) {
+//             val_kmer = read_slot(slot);
+//             if (val_kmer.kmer == key_kmer) {
+//                 success = true;
+//             }
+//         }
+//         ++probe;
+//     } while (!success && probe < size());
+//     return success;
+// }
 
-bool HashMap::slot_used(uint64_t slot) {
-    auto slot_ptr = used[slot/stride_]+slot%stride_;
-    return upcxx::rget(slot_ptr).wait() != 0;
-}
+// bool HashMap::slot_used(uint64_t slot) {
+//     auto slot_ptr = used[slot/stride_]+slot%stride_;
+//     return upcxx::rget(slot_ptr).wait() != 0;
+// }
 
-void HashMap::write_slot(uint64_t slot, const kmer_pair& kmer) {
-    auto slot_ptr = data[slot/stride_]+slot%stride_;
-    upcxx::rput(kmer, slot_ptr).wait();
-}
+// void HashMap::write_slot(uint64_t slot, const kmer_pair& kmer) {
+//     auto slot_ptr = data[slot/stride_]+slot%stride_;
+//     upcxx::rput(kmer, slot_ptr).wait();
+// }
 
-kmer_pair HashMap::read_slot(uint64_t slot) {
-    auto slot_ptr = data[slot/stride_]+slot%stride_;
-    return upcxx::rget(slot_ptr).wait();
-}
+// kmer_pair HashMap::read_slot(uint64_t slot) {
+//     auto slot_ptr = data[slot/stride_]+slot%stride_;
+//     return upcxx::rget(slot_ptr).wait();
+// }
 
-bool HashMap::request_slot(uint64_t slot) {
-    auto slot_ptr = used[slot/stride_]+slot%stride_;
-    int slot_val = ad_.fetch_add(slot_ptr, 1, std::memory_order_relaxed).wait();
-    return (slot_val == 0);
-}
+// bool HashMap::request_slot(uint64_t slot) {
+//     auto slot_ptr = used[slot/stride_]+slot%stride_;
+//     int slot_val = ad_.fetch_add(slot_ptr, 1, std::memory_order_relaxed).wait();
+//     return (slot_val == 0);
+// }
 
-size_t HashMap::size() const noexcept { return size_g_; }
+// size_t HashMap::size() const noexcept { return size_g_; }
