3e928f89180c820e2c5929c9b82a18623b2a7602
Sahil-Patil-23 <140925512+Sahil-Patil-23@users.noreply.github.com>
2025-04-13 20:35:50 -0700

    Update hash_map.hpp

--- hash_map.hpp
+++ hash_map.hpp
@@ -2,6 +2,7 @@
 
 #include "kmer_t.hpp"
 #include <upcxx/upcxx.hpp>
+using namespace std;
 
 struct HashMap {
     template<typename T>
@@ -11,7 +12,7 @@ struct HashMap {
 
     size_t my_size;
 
-    size_t size() const noexcept { return size_g_; }
+    size_t size() const noexcept { return global_size; }
 
     HashMap(size_t size);
 
@@ -51,105 +52,60 @@ struct HashMap {
 
     // Write and read to a logical data slot in the table.
     void write_slot(uint64_t slot, const kmer_pair& kmer){
-        auto slot_ptr = data[slot/stride_]+slot%stride_;
-        upcxx::rput(kmer, slot_ptr).wait();
+
+        auto slot_pointer = data[slot / stride] + (slot%stride);
+        upcxx::rput(kmer, slot_pointer).wait();
     }
 
     kmer_pair read_slot(uint64_t slot){
-        auto slot_ptr = data[slot/stride_]+slot%stride_;
-        return upcxx::rget(slot_ptr).wait();    
+
+        auto slot_pointer = data[slot / stride] + (slot%stride);
+        return upcxx::rget(slot_pointer).wait();    
     }
 
     // Request a slot or check if it's already used.
     bool request_slot(uint64_t slot){
-        auto slot_ptr = used[slot/stride_]+slot%stride_;
-        int slot_val = ad_.fetch_add(slot_ptr, 1, std::memory_order_relaxed).wait();
+        int slot_val;
+
+        auto slot_pointer = used[slot / stride] + (slot%stride);
+        slot_val = atomic_flags.fetch_add(slot_pointer, 1, memory_order_relaxed).wait();
         return (slot_val == 0);
     }
 
     bool slot_used(uint64_t slot){
-        auto slot_ptr = used[slot/stride_]+slot%stride_;
-        return upcxx::rget(slot_ptr).wait() != 0;
+
+        auto slot_pointer = used[slot / stride] + (slot%stride);
+        return upcxx::rget(slot_pointer).wait() != 0;
     }
 
 private:
-    int N_g_, n_;
-    size_t stride_, offset_, size_g_;
+    int world_size, rank;
+    size_t stride, offset, global_size;
     distributed<kmer_pair>* data_g_;
-    distributed<int>* used_g_;
-    upcxx::atomic_domain<int> ad_;
+    distributed<int>* slot_flag_ptr;
+    upcxx::atomic_domain<int> atomic_flags;
 };
 
 HashMap::HashMap(size_t size)
-    : ad_({upcxx::atomic_op::fetch_add}),
-    size_g_(size)
+    : atomic_flags({upcxx::atomic_op::fetch_add}),
+    global_size(size)
 {
-    N_g_ = upcxx::rank_n();
-    n_ = upcxx::rank_me();
-    stride_ = (size+N_g_-1)/N_g_;
-    offset_ = n_*stride_;
-    my_size = std::min(stride_, size-offset_);
+    world_size = upcxx::rank_n();
+    rank = upcxx::rank_me();
+
+    stride = (size+ world_size - 1) / world_size;
+    offset = rank * stride;
+    my_size = min(stride, size-offset);
+
     data_g_ = new distributed<kmer_pair>(upcxx::new_array<kmer_pair>(my_size));
-    used_g_ = new distributed<int>(upcxx::new_array<int>(my_size));
-    data = new upcxx::global_ptr<kmer_pair>[N_g_];
-    used = new upcxx::global_ptr<int>[N_g_];
-    for (int i = 0; i < N_g_; ++i) {
+    slot_flag_ptr = new distributed<int>(upcxx::new_array<int>(my_size));
+    
+    data = new upcxx::global_ptr<kmer_pair>[world_size];
+    used = new upcxx::global_ptr<int>[world_size];
+
+    int i;
+    for (i = 0; i < world_size; i++) {
         data[i] = data_g_->fetch(i).wait();
-        used[i] = used_g_->fetch(i).wait();
+        used[i] = slot_flag_ptr->fetch(i).wait();
     }
 }
-
-// bool HashMap::insert(const kmer_pair& kmer) {
-//     uint64_t hash = kmer.hash();
-//     uint64_t probe = 0;
-//     bool success = false;
-//     do {
-//         uint64_t slot = (hash + probe) % size();
-//         success = request_slot(slot);
-//         if (success) {
-//             write_slot(slot, kmer);
-//         }
-//         ++probe;
-//     } while (!success && probe < size());
-//     return success;
-// }
-
-// bool HashMap::find(const pkmer_t& key_kmer, kmer_pair& val_kmer) {
-//     uint64_t hash = key_kmer.hash();
-//     uint64_t probe = 0;
-//     bool success = false;
-//     do {
-//         uint64_t slot = (hash + probe) % size();
-//         if (slot_used(slot)) {
-//             val_kmer = read_slot(slot);
-//             if (val_kmer.kmer == key_kmer) {
-//                 success = true;
-//             }
-//         }
-//         ++probe;
-//     } while (!success && probe < size());
-//     return success;
-// }
-
-// bool HashMap::slot_used(uint64_t slot) {
-//     auto slot_ptr = used[slot/stride_]+slot%stride_;
-//     return upcxx::rget(slot_ptr).wait() != 0;
-// }
-
-// void HashMap::write_slot(uint64_t slot, const kmer_pair& kmer) {
-//     auto slot_ptr = data[slot/stride_]+slot%stride_;
-//     upcxx::rput(kmer, slot_ptr).wait();
-// }
-
-// kmer_pair HashMap::read_slot(uint64_t slot) {
-//     auto slot_ptr = data[slot/stride_]+slot%stride_;
-//     return upcxx::rget(slot_ptr).wait();
-// }
-
-// bool HashMap::request_slot(uint64_t slot) {
-//     auto slot_ptr = used[slot/stride_]+slot%stride_;
-//     int slot_val = ad_.fetch_add(slot_ptr, 1, std::memory_order_relaxed).wait();
-//     return (slot_val == 0);
-// }
-
-// size_t HashMap::size() const noexcept { return size_g_; }
